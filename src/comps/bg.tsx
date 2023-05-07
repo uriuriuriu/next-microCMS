bca0ba42bfab2f00de6209203c2b12805da0554b
uriuriuriu <uriuriuriugmail.com>
2023-05-08 03:24:14 +0900

    fix err

--- src/comps/bg.tsx
+++ src/comps/bg.tsx
@@ -1,9 +1,13 @@
 "use client";
+import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 
 export default function Bg() {
   // ページの生成された時間を取得
-  const time = new Date().toLocaleString();
+  const [time, setTime] = useState("");
+  useEffect(() => {
+    setTime(new Date().toLocaleString());
+  }, []);
   const pathName = usePathname();
   return (
     <p>
