649c840afe173354002c7b66353ac836467c6fa1
uriuriuriu <uriuriuriugmail.com>
2023-05-08 21:38:12 +0900

    add api

--- src/types/cms-types.ts
+++ src/types/cms-types.ts
@@ -17,6 +17,15 @@ type Structure<T, P> = T extends 'get'
   ? GetsType<{ id: string } & DateType & Required<P>>
   : Partial<DateType> & (T extends 'patch' ? Partial<P> : P);
 
+export type categories<T='get'> = Structure<
+T,
+{
+  /**
+   * カテゴリ名
+   */
+  name?: string
+}>
+
 export type blogs<T='get'> = Structure<
 T,
 {
@@ -41,18 +50,23 @@ T,
 
 export interface EndPoints {
   get: {
+    'categories': categories<'get'>
     'blogs': blogs<'get'>
   }
   gets: {
+    'categories': categories<'gets'>
     'blogs': blogs<'gets'>
   }
   post: {
+    'categories': categories<'post'>
     'blogs': blogs<'post'>
   }
   put: {
+    'categories': categories<'put'>
     'blogs': blogs<'put'>
   }
   patch: {
+    'categories': categories<'patch'>
     'blogs': blogs<'patch'>
   }
 }
