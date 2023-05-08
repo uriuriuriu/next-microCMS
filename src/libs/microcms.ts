d816ed1d17bae49b4e2812748a46b075ee013f0b
uriuriuriu <uriuriuriugmail.com>
2023-05-08 23:57:30 +0900

    fix api req2

--- src/libs/microcms.ts
+++ src/libs/microcms.ts
@@ -1,25 +1,39 @@
-import { createClient } from 'microcms-js-sdk';
-import type { MicroCMSQueries } from 'microcms-js-sdk';
-import { blogs } from '../types/microcms';
+import { blogs, EndPoints } from '../types/microcms';
+import { MicroCMS } from 'microcms-lib';
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 export type Blog = blogs;
+interface GetOptions<T> {
+  draftKey?: string;
+  fields?: ReadonlyArray<T>;
+  depth?: number;
+  globalKey?: boolean;
+}
+interface GetsOptions<T> {
+  draftKey?: string;
+  limit?: number;
+  offset?: number;
+  orders?: string;
+  q?: string;
+  fields?: T[];
+  ids?: string;
+  filters?: string;
+  depth?: number;
+  globalKey?: boolean;
+}
 
 if (!process.env.MICROCMS_SERVICE_DOMAIN)
   throw new Error('MICROCMS_SERVICE_DOMAIN is required');
 if (!process.env.MICROCMS_API_KEY)
   throw new Error('MICROCMS_API_KEY is required');
 
-export const client = createClient({
-  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+const cms = new MicroCMS<EndPoints>({
+  service: process.env.MICROCMS_SERVICE_DOMAIN,
   apiKey: process.env.MICROCMS_API_KEY,
 });
 
-export const getList = async (queries?: MicroCMSQueries) => {
-  const listData = await client.getList<Blog>({
-    endpoint: 'blogs',
-    queries,
-  });
+export const getList = async (queries?: GetsOptions<keyof Blog>) => {
+  const listData = await cms.gets('blogs', queries);
   await sleep(1000);
   return listData;
 };
@@ -27,13 +41,9 @@ export type BlogResponse = ReturnType<typeof getList>;
 
 export const getDetail = async (
   contentId: string,
-  queries?: MicroCMSQueries
+  queries?: GetOptions<keyof Blog>
 ) => {
-  const detailData = await client.getListDetail<Blog>({
-    endpoint: 'blogs',
-    contentId,
-    queries,
-  });
+  const detailData = await cms.get('blogs', contentId, queries);
   await sleep(1000);
   return detailData;
 };
